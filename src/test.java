271e5f466a488d0a4cce717ed982fbc3938d08f4
JordyWang <jordy999@163.com>
2022-05-08 14:35:30 +0800

    wyz第二天工作

--- src/test.java
+++ src/test.java
@@ -3,6 +3,7 @@ public class test {
         System.out.println("第一天工作完成");
         System.out.println("第一天加班");
         System.out.println("第一天加班zps");
+        System.out.println("wyz第二天工作");
 
     }
 }
